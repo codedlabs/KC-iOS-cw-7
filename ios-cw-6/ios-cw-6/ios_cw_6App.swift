a68427ee7f2b7b349cf864c125fbceb321ae3f3a
Retajalotaibi <rtooguae@gmail.com>
2022-03-04 20:08:51 +0300

    added an xcode project

---
+++ ios-cw-6/ios-cw-6/ios_cw_6App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cw_6App.swift
+//  ios-cw-6
+//
+//  Created by Retaj Al-Otaibi on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_cw_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

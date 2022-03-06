9faeea847c68828e2671d1e66fd3b086cb134d27
Bashaier AlMeshaileh <56195895+bkmushaileh@users.noreply.github.com>
2022-03-06 21:31:09 +0300

    adding textfield classwork with xcode file

---
+++ ios-cw-6/TextFieldClassWork/TextFieldClassWork/TextFieldClassWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldClassWorkApp.swift
+//  TextFieldClassWork
+//
+//  Created by Bashaier Almeshaileh on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldClassWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
